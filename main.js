019b6bf68eab6d30ea11df0c05703ac6a7de1ac6
Auram20 <myriamanik97@gmail.com>
2018-07-24 13:03:05 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+  var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 40;
+    traY = ((4 * mouseY) / 570) + 50;
+    console.log(traX);
+    $(".title").css({"background-position": traX + "%" + traY + "%"});
+  });
+});
+
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
+
+
+
+/////////
+
+(function($){
+  // variables
+  elementWidth = $('ul').width(),
+  containerWidth = $('nav').width(),
+  difference = elementWidth-containerWidth,
+  finalWidth = difference * 1.5,
+  element = $('ul');
+  
+  // active on click
+  $('li').on('click', function(){
+    $('li').removeClass('active');
+    $(this).addClass('active');
+  });
+  
+})(jQuery);
+
+
+////////
+
+$("#monimage1").click(function() {
+  $('.transform').toggleClass('transform-active');
+});
+
+
+$("#monimage1").click(function() {
+  $('.firstnav').toggleClass('firstnav-active');
+});
+
+
+$("#monimage1").click(function() {
+  $('.slide-particle').toggleClass('slide-particle-active');
+});
+
+/////
+
+
+function delay (URL) {
+    setTimeout( function() { window.location = URL }, 2000 );
+}
